12691516feaa10f0779abb770482a660314ec77e
Olufemi Salaam <osalaam@osalaam-macbookpro.roam.corp.google.com>
2017-08-02 21:46:14 -0700

    Navigation Complete

--- app/src/main/java/com/osalaam/immersionproj2/MainActivity.java
+++ app/src/main/java/com/osalaam/immersionproj2/MainActivity.java
@@ -1,8 +1,28 @@
 package com.osalaam.immersionproj2;
 
-/**
- * Created by osalaam on 8/2/17.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private Button mClassesButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mClassesButton = (Button) findViewById(R.id.loginbutton);
+
+        mClassesButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, ClassesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
