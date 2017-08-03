d05a22bff2acee4646395c0230846f71cb532a2b
Olufemi Salaam <osalaam@osalaam-macbookpro.roam.corp.google.com>
2017-08-02 23:28:04 -0700

    Search Partial Functionality

--- app/src/main/java/com/osalaam/immersionproj2/CoursesActivity.java
+++ app/src/main/java/com/osalaam/immersionproj2/CoursesActivity.java
@@ -1,8 +1,12 @@
 package com.osalaam.immersionproj2;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +16,7 @@ import android.widget.ListView;
 
 public class CoursesActivity  extends AppCompatActivity {
     private ListView mListView;
+    private SearchView searchView;
     private String[] mCourses = {"CSCI 101", "CSCI 241"};
 
     @Override
@@ -19,10 +24,15 @@ public class CoursesActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) findViewById(R.id.search);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         mListView = (ListView) findViewById(R.id.list);
         ListAdapter course_list = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mCourses);
         mListView.setAdapter(course_list);
-        int count = mListView.getChildCount();
+
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -30,6 +40,14 @@ public class CoursesActivity  extends AppCompatActivity {
                     startActivity(intent);
                 }
         });
+
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i("OOS", query);
+        }
+
 
     }
 
--- app/src/main/java/com/osalaam/immersionproj2/ResourcesActivity.java
+++ app/src/main/java/com/osalaam/immersionproj2/ResourcesActivity.java
@@ -1,7 +1,12 @@
 package com.osalaam.immersionproj2;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 
 public class ResourcesActivity extends AppCompatActivity {
 
@@ -10,6 +15,17 @@ public class ResourcesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) findViewById(R.id.search);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i("OOS", query);
+        }
         /*mClassesButton = (Button) findViewById(R.id.main);
         mClassesButton.setOnClickListener(new View.OnClickListener() {
             @Override
