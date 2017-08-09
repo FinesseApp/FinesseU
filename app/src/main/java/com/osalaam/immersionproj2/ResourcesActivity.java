bff2a2a6d9ce9f918c4b8a3a5e18379c0d070f7c
Olufemi Salaam <osalaam@osalaam-macbookpro.roam.corp.google.com>
2017-08-09 11:03:47 -0700

    Intents extra passed to Resources activity

--- app/src/main/java/com/osalaam/immersionproj2/ResourcesActivity.java
+++ app/src/main/java/com/osalaam/immersionproj2/ResourcesActivity.java
@@ -3,19 +3,23 @@ package com.osalaam.immersionproj2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class ResourcesActivity extends AppCompatActivity {
 
     Button mUpload;
+    String className;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
 
+        className = getIntent().getStringExtra("class_name");
 
         mUpload = (Button) findViewById(R.id.uploading);
+        Log.i("CLASS-----------", className);
 
         mUpload.setOnClickListener(new View.OnClickListener(){
             @Override
